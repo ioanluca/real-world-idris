97b4eb221535b6796af4a7d7d2af0cc475530bb1
Ioan Luca <ioan.luca7@gmail.com>
2019-02-28 23:38:01 +0000

    move to text

--- src/Malfunction/AST.hs
+++ src/Malfunction/AST.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Malfunction.AST
     ( defaultCase
     )
 where
+
+import qualified Data.Text                     as T
 
 -- data MlfProgram
 --     = Interpreted [MlfBinding] MlfExp
@@ -105,73 +109,77 @@ data MlfExp
     | MlfComment String
     deriving (Eq, Show)
 
-mlfAST2Text :: MlfExp -> String
-mlfAST2Text e = go e ""
+textShow :: Show a => a -> T.Text
+textShow = T.pack . show
 
-ip :: String -> String
-ip e = '(' : e ++ ") "
+ip :: T.Text -> T.Text
+ip e = T.concat ["(", e, ")"]
 
-arithType2Text :: MlfArithType -> String
+arithType2Text :: MlfArithType -> T.Text
 arithType2Text Int32Arith  = ".int32"
 arithType2Text Int64Arith  = ".int64"
-arithType2Text IntArith    = ""
+arithType2Text IntArith    = T.empty
 arithType2Text BigIntArith = ".ibig"
 arithType2Text FloatArith  = ".f64"
 
-go :: MlfExp -> String -> String
-go (MlfProg bindings e2) k = undefined
-go (MlfVar    e        ) k = '$' : e ++ k
+name2Text :: MlfName -> T.Text
+name2Text name = '$' `T.cons` T.pack name
 
-go (MlfInt32  e        ) k = show e ++ arithType2Text Int32Arith ++ k
-go (MlfInt64  e        ) k = show e ++ arithType2Text Int64Arith ++ k
-go (MlfInt    e        ) k = show e ++ k
-go (MlfBigInt e        ) k = show e ++ arithType2Text BigIntArith ++ k
-go (MlfFloat  e        ) k = show e ++ k
-go MlfPosInf             k = "infinity" ++ k
-go MlfNegInf             k = "neg_infinity" ++ k
-go MlfNaN                k = "nan"
+binding2Text :: MlfBinding -> T.Text
+binding2Text (RegBinding name e) =
+    ip $ T.concat [name2Text name, " ", mlfAST2Text e]
+binding2Text (ExecBinding e) = ip $ T.concat ["_", " ", mlfAST2Text e]
+binding2Text (RecBinding es) =
+    ip $ T.concat ["rec", " "] `T.append` T.concat (map binding2Text es)
 
-go (MlfConvert from to e) k =
-    ip $ "convert" ++ arithType2Text from ++ arithType2Text to ++ go e k ++ k
-
-go (MlfString e             ) k = undefined
-go (MlfPlus  e1 e2 e3       ) k = undefined
-go (MlfMinus e1 e2 e3       ) k = undefined
-go (MlfTimes e1 e2 e3       ) k = undefined
-go (MlfDiv   e1 e2 e3       ) k = undefined
-go (MlfMod   e1 e2 e3       ) k = undefined
-go (MlfNeg e1 e2            ) k = undefined
-go (MlfEq           e1 e2 e3) k = undefined
-go (MlfLT           e1 e2 e3) k = undefined
-go (MlfLTEq         e1 e2 e3) k = undefined
-go (MlfGT           e1 e2 e3) k = undefined
-go (MlfGTEq         e1 e2 e3) k = undefined
-go (MlfBitAnd       e1 e2 e3) k = undefined
-go (MlfBitOr        e1 e2 e3) k = undefined
-go (MlfBitXOr       e1 e2 e3) k = undefined
-go (MlfBitLShift    e1 e2 e3) k = undefined
-go (MlfBitRShift    e1 e2 e3) k = undefined
-go (MlfBitRShiftExt e1 e2 e3) k = undefined
-go (MlfLam e1 e2            ) k = undefined
-go (MlfApp e1 e2            ) k = undefined
-go (MlfLet e1 e2            ) k = undefined
-go (MlfSeq e                ) k = undefined
-go (MlfBlock      e1 e2     ) k = undefined
-go (MlfProjection e1 e2     ) k = undefined
-go (MlfVec     e1 e2 e3     ) k = undefined
-go (MlfVecLoad e1 e2 e3     ) k = undefined
-go (MlfVecStore e1 e2 e3 e4 ) k = undefined
-go (MlfVecLen e1 e2         ) k = undefined
-go (MlfLazy  e              ) k = undefined
-go (MlfForce e              ) k = undefined
-go (MlfSwitch e1 e2         ) k = undefined
-go (MlfIf e1 e2 e3          ) k = undefined
-go (MlfOCaml e1 e2          ) k = undefined
-
-    -- show sexp = render sexp ""   where
-    --     render :: Sexp -> String -> String
-    --     render (S       s) k = "(" ++ foldr render (") " ++ k) s
-    --     render (A       s) k = s ++ " " ++ k
-    --     render (KInt    n) k = show n ++ " " ++ k
-    --     render (KStr    s) k = show s ++ " " ++ k
-    --     render (KBigInt s) k = show s ++ ".ibig " ++ k
+mlfAST2Text :: MlfExp -> T.Text
+mlfAST2Text (MlfProg binds e) =
+    ip
+        $  T.concat
+        $  [ip "module", " "]
+        ++ map binding2Text binds
+        ++ [ip "export"]
+mlfAST2Text (MlfVar    e)              = name2Text e
+mlfAST2Text (MlfInt32  e)              = undefined
+mlfAST2Text (MlfInt64  e)              = undefined
+mlfAST2Text (MlfInt    e)              = undefined
+mlfAST2Text (MlfBigInt e)              = undefined
+mlfAST2Text (MlfFloat  e)              = undefined
+mlfAST2Text MlfPosInf                  = undefined
+mlfAST2Text MlfNegInf                  = undefined
+mlfAST2Text MlfNaN                     = undefined
+mlfAST2Text (MlfConvert e1 e2 e3     ) = undefined
+mlfAST2Text (MlfString e             ) = undefined
+mlfAST2Text (MlfPlus  e1 e2 e3       ) = undefined
+mlfAST2Text (MlfMinus e1 e2 e3       ) = undefined
+mlfAST2Text (MlfTimes e1 e2 e3       ) = undefined
+mlfAST2Text (MlfDiv   e1 e2 e3       ) = undefined
+mlfAST2Text (MlfMod   e1 e2 e3       ) = undefined
+mlfAST2Text (MlfNeg e1 e2            ) = undefined
+mlfAST2Text (MlfEq           e1 e2 e3) = undefined
+mlfAST2Text (MlfLT           e1 e2 e3) = undefined
+mlfAST2Text (MlfLTEq         e1 e2 e3) = undefined
+mlfAST2Text (MlfGT           e1 e2 e3) = undefined
+mlfAST2Text (MlfGTEq         e1 e2 e3) = undefined
+mlfAST2Text (MlfBitAnd       e1 e2 e3) = undefined
+mlfAST2Text (MlfBitOr        e1 e2 e3) = undefined
+mlfAST2Text (MlfBitXOr       e1 e2 e3) = undefined
+mlfAST2Text (MlfBitLShift    e1 e2 e3) = undefined
+mlfAST2Text (MlfBitRShift    e1 e2 e3) = undefined
+mlfAST2Text (MlfBitRShiftExt e1 e2 e3) = undefined
+mlfAST2Text (MlfLam e1 e2            ) = undefined
+mlfAST2Text (MlfApp e1 e2            ) = undefined
+mlfAST2Text (MlfLet e1 e2            ) = undefined
+mlfAST2Text (MlfSeq e                ) = undefined
+mlfAST2Text (MlfBlock      e1 e2     ) = undefined
+mlfAST2Text (MlfProjection e1 e2     ) = undefined
+mlfAST2Text (MlfVec     e1 e2 e3     ) = undefined
+mlfAST2Text (MlfVecLoad e1 e2 e3     ) = undefined
+mlfAST2Text (MlfVecStore e1 e2 e3 e4 ) = undefined
+mlfAST2Text (MlfVecLen e1 e2         ) = undefined
+mlfAST2Text (MlfLazy  e              ) = undefined
+mlfAST2Text (MlfForce e              ) = undefined
+mlfAST2Text (MlfSwitch e1 e2         ) = undefined
+mlfAST2Text (MlfIf e1 e2 e3          ) = undefined
+mlfAST2Text (MlfOCaml e1 e2          ) = undefined
+mlfAST2Text (MlfComment e            ) = undefined
