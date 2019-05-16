7e9de46db7792ad1befcd7046cadd49a1002fa2f
Ioan Luca <ioan.luca7@gmail.com>
2019-05-16 09:00:48 +0100

    [ fix ] pass OCaml_IO functions with multiple arguments

--- src/IRTS/OCamlFFI.hs
+++ src/IRTS/OCamlFFI.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-} 
+{-# LANGUAGE PatternSynonyms #-}
+
 module IRTS.OCamlFFI where
 
 import           Malfunction.AST
@@ -6,20 +8,24 @@ import           IRTS.Lang
 import           Idris.Core.TT                  ( Name(UN) )
 import           Data.List.Split                ( splitOn )
 
+pattern OCaml_FunT f <- FApp (UN "OCaml_FnT") [_, f]
+pattern OCaml_Fn t f   <- FApp (UN "OCaml_Fn") [_, _, t, f]
+pattern OCaml_FnBase t <- FApp (UN "OCaml_FnBase") [_, t]
+pattern OCaml_FnIO t   <- FApp (UN "OCaml_FnIO") [_, _, t]
+
 makeForeignCall :: FDesc -> FDesc -> [FDesc] -> [MlfExp] -> MlfExp
 makeForeignCall ret (FStr fn) []  []   = stdLib (splitOn "." fn)
 makeForeignCall ret (FStr fn) fds args = stdLibCall splits
   $ zipWith cgForeignArg fds args
   where splits = splitOn "." fn
 
-
 cgForeignArg :: FDesc -> MlfExp -> MlfExp
-cgForeignArg (FApp (UN "OCaml_FnT") [_, FApp (UN "OCaml_Fn") [_, _, a, r]]) f =
-  let x    = cgForeignArg a (MlfVar "x")
-      body = MlfApp f [x]
-  in  MlfLam ["x"] body
-cgForeignArg (FApp (UN "OCaml_FnT") [_, FApp (UN "OCaml_Fn") [_, _, a, FApp (UN "OCaml_FnIO") [_, _, b]]]) f
-  = let x    = cgForeignArg a (MlfVar "x")
-        body = MlfApp f [x, MlfNothing]
-    in  MlfLam ["x"] body
+cgForeignArg (OCaml_FunT f) arg = go f 0
+  where go (OCaml_Fn t fn) n = go fn (n + 1)
+        go (OCaml_FnBase t) n = arg
+        go (OCaml_FnIO t) n = 
+          let as = map textShow [1..n]
+              body = MlfApp arg ((map MlfVar as) ++ [MlfNothing])
+          in MlfLam as body
+        go _ n = arg
 cgForeignArg _ arg = arg
